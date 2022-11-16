52e829c407cc43bb9fe98cbbbd6e4a906b6629b7
tan <tan.nguyennhut@gotadi.com>
2022-11-16 21:53:19 +0700

    src

--- src/components/WrapperBox.js
+++ src/components/WrapperBox.js
@@ -2,9 +2,9 @@ import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const WrapperBox = styled(Box)(({ theme }) => ({
-    border: `1px solid ${theme.palette.primary.main}`,
-    boxShadow: "2px 2px 7px 1px #95a5a6",
-    textAlign: "center"
-}))
+  border: `1px solid ${theme.palette.primary.main}`,
+  boxShadow: "2px 2px 7px 1px #95a5a6",
+  textAlign: "center",
+}));
 
-export default WrapperBox
+export default WrapperBox;
